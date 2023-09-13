694648a15d1fa74f11a049da1d1f2cb68bb73657
cimmer1 <cimmer@mail.bradley.edu>
2023-09-13 10:29:09 -0500

    created game manager script

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // public int Health;
+    // public int Honey;
+    // public GameObject StartWaveButton;    know when wave has ended, know when
+    // wave has started, keep track of health & currency
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
