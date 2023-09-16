08dcf2014fbeb67d9b54d11f217c539ca36dd538
DrewForYou <andrewdrees214@yahoo.com>
2023-09-15 20:02:52 -0500

    Movement Checkpoint A
    Can move from start to first point

--- Assets/Sprites/Enemeis/EnemyAI.cs
+++ Assets/Sprites/Enemeis/EnemyAI.cs
@@ -1,6 +1,7 @@
 using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class EnemyAI : MonoBehaviour
@@ -12,12 +13,36 @@ public class EnemyAI : MonoBehaviour
     public Rigidbody2D EnemyRB;
     public Collider2D EnemyCollider;
     public List<GameObject> Pathing = new List<GameObject>();
+    private int currentTarget;
 
+    private void Awake()
+    {
+        EnemyMove();
+    }
+
+    //Moves the enemy
     public void EnemyMove()
     {
+        StartCoroutine(MoveMe(Pathing[0]));
         //Set up corotine that takes a positoin from the list and runs till the enemy reaches that point.
     }
 
+    //Goes through and moves the enemy to the current target
+    private IEnumerator MoveMe(GameObject goal)
+    {
+        float step = Time.deltaTime * Speed;
+        while(transform.position != goal.transform.position)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, goal.transform.position, step);
+            print("" + transform.position);
+            print("Goal " + goal.transform.position);
+            yield return null;
+        }
+        //Pathing.Remove(goal);
+        //EnemyMove();
+    }
+
+    //Damages the enemy and checks if it has died
     public void Damaged(int damage)
     {
         Health -= damage;
