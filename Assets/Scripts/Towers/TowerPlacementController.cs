b7bf4305f93a8a8f2928736ee9c0de6d440964aa
cimmer1 <cimmer@mail.bradley.edu>
2023-09-17 15:55:45 -0500

    Created some placeholder UI for Towers. Also made a script for placing towers

---
+++ Assets/Scripts/Towers/TowerPlacementController.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerPlacementController : MonoBehaviour
+{
+    private GameObject warriorPrefab;
+    private GameObject toBuild;
+
+}
