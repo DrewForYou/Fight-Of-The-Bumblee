60ce72da1810a9f655ca2044638455a9913f218b
DrewForYou <andrewdrees214@yahoo.com>
2023-10-02 10:42:19 -0500

    Enemies fixed

--- Assets/Scripts/Towers/Mage Bee/Attacks/MageBeeFireball.cs
+++ Assets/Scripts/Towers/Mage Bee/Attacks/MageBeeFireball.cs
@@ -6,17 +6,6 @@ public class MageBeeFireball : MonoBehaviour
 {
     public int Damage;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Enemy")
