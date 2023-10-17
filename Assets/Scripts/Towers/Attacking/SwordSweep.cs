fd967d213ef8286d096a3e887fb65798037f3214
Charmancer <coshaughnessy@mail.bradley.edu>
2023-10-16 22:33:20 -0500

    fill out warrior bee upgrades

--- Assets/Scripts/Towers/Attacking/SwordSweep.cs
+++ Assets/Scripts/Towers/Attacking/SwordSweep.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class SwordSweep : MonoBehaviour
 {
-    public int Damage;
+   
     public int Hits=3;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        WarriorBeeCode wb = GameObject.FindObjectOfType<WarriorBeeCode>();
         if (collision.gameObject.tag == "Enemy")
         {
             
-            collision.GetComponent<EnemyAI>().Damaged(Damage);
+            collision.GetComponent<EnemyAI>().Damaged(wb.Damage);
             if(Hits <= 0)
             {
                 Destroy(this.gameObject);
