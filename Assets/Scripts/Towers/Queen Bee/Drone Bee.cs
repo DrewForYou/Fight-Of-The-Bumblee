f742fe20c4820d43a6236fd346408e7c7b7af4fb
DrewForYou <andrewdrees214@yahoo.com>
2023-10-25 10:01:28 -0500

    2x speed added

--- Assets/Scripts/Towers/Queen Bee/Drone Bee.cs
+++ Assets/Scripts/Towers/Queen Bee/Drone Bee.cs
@@ -4,15 +4,62 @@ using UnityEngine;
 
 public class DroneBee : MonoBehaviour
 {
+    public int Strength;
+    public int Value;
+    public float Speed;
+    public Rigidbody2D DroneRB;
+    public Collider2D DroneCollider;
+    public List<GameObject> Pathing;
+    public GameManager GameManager;
+    public WaveManager WaveManager;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameManager = FindAnyObjectByType<GameManager>();
+        WaveManager = FindAnyObjectByType<WaveManager>();
+
+        Pathing = new List<GameObject>(GameManager.MapPoints);
+        Pathing.Reverse();
+        EnemyMove();
     }
 
-    // Update is called once per frame
-    void Update()
+    //Moves the enemy
+    public void EnemyMove()
     {
-        
+        StartCoroutine(MoveMe(Pathing[0]));
+
+        //Set up corotine that takes a positoin from the list and runs till the enemy reaches that point.
+    }
+
+    //Goes through and moves the enemy to the current target
+    private IEnumerator MoveMe(GameObject goal)
+    {
+        while (Vector3.Distance(transform.position, goal.transform.position) > 0.2f)
+        {
+            if (GameManager.IsRunning)
+            {
+                transform.position = Vector2.MoveTowards(transform.position, goal.transform.position, Time.deltaTime * Speed);
+            }
+            yield return null;
+        }
+
+        //print(Pathing.Count);
+        Pathing.Remove(goal);
+
+        if (Pathing.Count > 0)
+        {
+            EnemyMove();
+        }
+        else
+        {
+            ReachedEnd();
+        }
+    }
+
+    public void ReachedEnd()
+    {
+        GameManager.Hurt(Strength);
+        Destroy(gameObject);
     }
 }
